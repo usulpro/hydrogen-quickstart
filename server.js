848475c6aaf0531eba8050607e1a7a3cbb57a20d
Oleg Proskurin <oleg@gitnation.org>
2024-09-05 18:37:19 +0700

    Revert "reproduce caching behavior"
    This reverts commit b7de67d661d158c3811f002d697733c2d1ab8348.

--- server.js
+++ server.js
@@ -1,29 +1,65 @@
-const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Page</title>
-</head>
-<body>
-    <h1>Hello, Caleffi!</h1>
-    <p>This is a simple cached HTML page.</p>
-</body>
-</html>`;
+// @ts-ignore
+// Virtual entry point for the app
+import * as remixBuild from 'virtual:remix/server-build';
+import {storefrontRedirect} from '@shopify/hydrogen';
+import {createRequestHandler} from '@shopify/remix-oxygen';
+import {createAppLoadContext} from '~/lib/context';
 
+/**
+ * Export a fetch handler in module format.
+ */
 export default {
-  async fetch(request) {
-    const responseHeaders = new Headers({
-      'Content-Type': 'text/html',
-      'Cache-Control': 'public, max-age=31536000, stale-while-revalidate=3600',
-      'Oxygen-Cache-Control': 'public, max-age=43200, stale-while-revalidate=3600',
-      Vary: 'Accept-Encoding',
-    });
+  /**
+   * @param {Request} request
+   * @param {Env} env
+   * @param {ExecutionContext} executionContext
+   * @return {Promise<Response>}
+   */
+  async fetch(request, env, executionContext) {
+    try {
+      const appLoadContext = await createAppLoadContext(
+        request,
+        env,
+        executionContext,
+      );
 
-    console.log('🚀 ~ fetch ~ request.url:', request.url);
-    return new Response(html, {
-      headers: responseHeaders,
-      status: 200,
-    });
+      /**
+       * Create a Remix request handler and pass
+       * Hydrogen's Storefront client to the loader context.
+       */
+      const handleRequest = createRequestHandler({
+        build: remixBuild,
+        mode: process.env.NODE_ENV,
+        getLoadContext: () => appLoadContext,
+      });
+
+      const response = await handleRequest(request);
+
+      if (appLoadContext.session.isPending) {
+        response.headers.set(
+          'Set-Cookie',
+          await appLoadContext.session.commit(),
+        );
+      }
+
+      if (response.status === 404) {
+        /**
+         * Check for redirects only when there's a 404 from the app.
+         * If the redirect doesn't exist, then `storefrontRedirect`
+         * will pass through the 404 response.
+         */
+        return storefrontRedirect({
+          request,
+          response,
+          storefront: appLoadContext.storefront,
+        });
+      }
+
+      return response;
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return new Response('An unexpected error occurred', {status: 500});
+    }
   },
 };
